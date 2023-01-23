417383b006de98dab8f961bd4be6f6b3cb903171
MamaKbubu <maboelatk@gmail.com>
2023-01-23 11:09:06 -0800

    no

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print(f"{:d}",value)
+        print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
         return False
+
