64a4e0a4bf743ba4dc088bdb333daeaff3d02aab
MamaKbubu <111245031+MamaKbubu@users.noreply.github.com>
2023-01-23 19:23:53 +0200

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    sum = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
-            i += 1
+            sum += 1
         except ValueError:
             break
         print()
