22a3623ed8da065db588f873ea9f040d9acaca6b
MamaKbubu <maboelatk@gmail.com>
2023-01-31 06:49:45 -0800

    1st

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Defines a square """
+
+
 class Square:
-    """Defines a square"""
+    """Represents a square"""
+
     def __init__(self, size):
-        """Initialises the data"""
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
         self.__size = size
