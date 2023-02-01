5ceade9ee5db6e10d01824aee02962535222c79f
MamaKbubu <maboelatk@gmail.com>
2023-01-31 20:38:12 -0800

    mistake

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,38 @@
 #!/usr/bin/python3
 """ A Class that defines a rectangle """
 
-Class Rectangle:
-    """ An empty class that presents a rectangle """
-    pass
+class Rectangle:
+    """A class to define a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize the instance"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
