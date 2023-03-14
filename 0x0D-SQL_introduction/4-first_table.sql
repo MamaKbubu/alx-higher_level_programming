d8a146fbc31c7629a68b1bce19f463ff73b004b0
MamaKbubu <maboelatk@gmail.com>
2023-03-14 14:49:30 -0700

    yes

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,9 +1,9 @@
 -- creates the table which will be called First_table on the current database
 -- The database name will be passed as an argument of my sql command
 --		id INT
---		name VARCHAR(200)
+--		name VARCHAR(256)
 -- the database name wil be passed as an argument of mysql command
 -- If the table First_table already exist, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
 
-CREATE TABLE IF NOT First_table (id INT, name VARCHAR(200));
+CREATE TABLE IF NOT First_table (id INT, name VARCHAR(256));
