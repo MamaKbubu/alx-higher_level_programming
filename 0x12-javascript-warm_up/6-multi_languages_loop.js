6abf29ac2100d0600851c20bf150476c388891b9
MamaKbubu <maboelatk@gmail.com>
2023-04-11 16:23:53 -0700

    task 6 attempt

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// This code literally has 3 sentences, you;ll see which one you print
+//
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+myVar.forEach((element) => {
+  console.log(element);
+});
