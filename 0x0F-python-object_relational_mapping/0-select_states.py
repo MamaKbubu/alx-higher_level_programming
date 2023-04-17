6e24aba49ed129ae82cab8040099cd1635131430
MamaKbubu <maboelatk@gmail.com>
2023-04-17 06:10:29 +0200

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,20 @@ import sys
 '''This code will get all states from the database hbtn_0e_0_usa'''
 
 if __name__ == "__main__":
+    '''First line of code will connect to MySQLserver running on localhost on port 3306 '''
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    '''Second line of code will help you navigate on the database '''
+    cur = db.cursor
+    '''This line will help you to pin point what to execute'''
+    cur.execute("SELECT *FROM states ORDER by id ASC")
+    '''This function allows for all the information to be fetched and placed in rows'''
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    '''This line is a for loop that will run for rows'''
+    for rows in rows:
+        '''print the rows'''
+        print(rows)
+    '''close the cursor'''
     cur.close()
+    '''close the db'''
     db.close()
