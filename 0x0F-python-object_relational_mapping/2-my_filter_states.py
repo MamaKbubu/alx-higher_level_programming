12d5cd34f0be1a00eaa1cc41fc3477d559011271
MamaKbubu <maboelatk@gmail.com>
2023-04-21 18:48:09 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,44 +1,27 @@
 #!/usr/bin/python3
-'''
-    This script takes in an
-    argument and displays all
-    values in the states table
-    of hbtn_0e_0_usa where name
-    matches the argument.
-'''
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
 
-import MySQLdb
-import sys
+import MySQLdb as db
+from sys import argv
 
-if __name__ == "__main__":
-    # Get command line args
-    u_name = sys.argv[1]
-    sql_pass = sys.argv[2]
-    sql_db = sys.argv[3]
-    state_name = sys.argv[4]
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
 
-    # Connect to DB
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=u_name,
-        passwd=sql_pass,
-        db=sql_db
-    )
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+                        states.id ASC".format(argv[4]))
+    rows_selected = db_cursor.fetchall()
 
-    # Create cursor object
-    cursor = db.cursor()
-
-    # Execute SQL Query
-    cursor.execute("SELECT * \
-                   FROM states \
-                   WHERE name = '{state}' \
-                   ORDER BY id ASC".format(state=state_name))
-
-    # Fetch all rows and print them
-    rows = cursor.fetchall()
-    for row in rows:
+    for row in rows_selected:
         print(row)
-
-    # Close db connection
-    db.close()
